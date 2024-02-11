8190f0573e5e3b6a88acf93ec1257e4c55d62d39
Andrei-George Lichi <lichiag21@gmail.com>
2024-02-11 23:31:52 +0100

    Gold counter added

--- RollABall/Assets/Scripts/PlayerController.cs
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    public TextMeshProUGUI goldCount;
     public float speed = 0;
+    private int gold;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        gold = 0;
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+     goldCount.text = "Gold:" + gold.ToString();
     }
     void FixedUpdate()
     {
@@ -21,12 +31,13 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("PickUp")) 
         {
           other.gameObject.SetActive(false);
+          gold++;
+          SetCountText();
         }
     }
 }
